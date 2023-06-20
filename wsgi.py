df9b2b6bead12bc22a656acc8212a8d17ce69f72
Moniquecho <64466887+Moniquecho@users.noreply.github.com>
2023-06-20 10:46:44 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from main import app
 
-if __name__ == "__main__":
-  app.run()
+def test_index():
+    response = app.test_client().get('/')
+
+    assert response.data == b'Hello, It is a nice weather!'
+    assert response.status_code == 200
